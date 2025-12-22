7d0c21867fbcbb828e3597058f0cc7f84507e1ad
Albert O'Shea <albertoshea2@gmail.com>
2025-12-22 15:42:14 +1100

    chore: bump version

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         };
         ripdocPkg = pkgs.rustPlatform.buildRustPackage {
           pname = "ripdoc";
-          version = "0.7.0";
+          version = "0.8.0";
           src = pkgs.lib.cleanSource ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
