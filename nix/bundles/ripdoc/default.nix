642cc87de79f9379362d987d360bc79f072de42a
Albert O'Shea <albertoshea2@gmail.com>
2026-01-24 15:26:56 +1100

    chore: use rust bundle's cargoOutputHashes config
    Move git dependency hashes to rust.config.nix, removing duplication
    from ripdoc bundle. Updates rust bundle submodule with new config
    option.

--- nix/bundles/ripdoc/default.nix
+++ nix/bundles/ripdoc/default.nix
@@ -134,10 +134,6 @@
       __outputs.perSystem.buildDeps.ripdoc = {
         inherit buildInputs nativeBuildInputs;
         env = commonEnv;
-        cargoOutputHashes = {
-          "cargo-manifest-0.19.1" = "sha256-sTHScYSlkCgYYpv9diaTnPfUBCDFuzjfkRRBi75F0g8=";
-          "rustdoc-json-0.9.7" = "sha256-mJiY2/X6aeT1LQwbuBp4Cpw5xzz5MAC44U6paCcQ77I=";
-        };
       };
     };
 }
